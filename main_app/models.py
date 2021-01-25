487faa604e7cf1ea199fce2d077720c2af9333cf
Mike Cao <mikecao369@gmail.com>
2021-01-25 14:51:53 -0500

    lab part.2

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Album(models.Model):
+    name = models.CharField(max_length=100)
+    artists = models.CharField(max_length=100)
+    genre = models.TextField(max_length=100)
+    year = models.IntegerField()
+    def __str__(self):
+        return self.name
--- main_app/views.py
+++ main_app/views.py
@@ -1,23 +1,22 @@
 from django.shortcuts import render
 
-# Add the following import
-from django.http import HttpResponse
+from .models import Album
 
 # Add the Cat class & list and view function below the imports
 
-class Album:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, name, artists, genre, year):
-    self.name = name
-    self.artists = artists
-    self.genre = genre
-    self.year = year
+# class Album:  # Note that parens are optional if not inheriting from another class
+#   def __init__(self, name, artists, genre, year):
+#     self.name = name
+#     self.artists = artists
+#     self.genre = genre
+#     self.year = year
 
-albums = [
-    Album('American Beauty', 'Grateful Dead', 'Folk Rock', 1970),
-    Album('\'Allelujah! Don\'t Bend! Ascend', 'Godspeed You! Black Emperor', 'Post-Rock', 2012),
-    Album('Microphones in 2020', 'The Microphones', 'Indie Folk', 2020),
-    Album('冀西南林路行', '万能青年旅店', 'Folk Rock', 2020)
-]
+# albums = [
+#     Album('American Beauty', 'Grateful Dead', 'Folk Rock', 1970),
+#     Album('\'Allelujah! Don\'t Bend! Ascend', 'Godspeed You! Black Emperor', 'Post-Rock', 2012),
+#     Album('Microphones in 2020', 'The Microphones', 'Indie Folk', 2020),
+#     Album('冀西南林路行', '万能青年旅店', 'Folk Rock', 2020)
+# ]
 
 def home(request):
   return render(request, 'home.html')
@@ -26,4 +25,9 @@ def about(request):
   return render(request, 'about.html')
 
 def albums_index(request):
+  albums = Album.objects.all()
   return render(request, 'albums/index.html', {'albums': albums})
+
+def albums_detail(request, album_id):
+  album = Album.objects.get(id=album_id)
+  return render(request, 'albums/detail.html', {'album': album})
